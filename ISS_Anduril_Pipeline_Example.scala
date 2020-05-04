a604c541682cb663b0e68cc0e3dc87f4040d500b
Gabriele <gabriele.partel@it.uu.se>
2020-05-04 15:20:19 +0200

    Fixed new conda dependencies for Anduril pipeline. Added jupyter pipeline.

--- ISS_Anduril_Pipeline_Example.scala
+++ ISS_Anduril_Pipeline_Example.scala
@@ -1,7 +1,7 @@
 #!/usr/bin/env anduril
 //$PRE startdate=$( date )
 //$POST echo -e "Running time:\n$startdate -> $( date )"
-//$OPT  --wrapper anduril-wrapper-slurm --threads 20
+//$OPT --threads 20
 //$OPT --java-heap 4096
 //$OPT -d ./result_ISS_Anduril_Pipeline_Example
 
@@ -24,15 +24,19 @@ object pipeline {
 
 	val img_array = Folder2Array(folder1=dataset_folder, keyMode="number", filePattern="(.*).tif", excludePattern=".*Transformed")
 	val img_CSV = Array2CSV(img_array)
-	val mode_percent = BashEvaluate(var1=prepro_pipeline_path, var2=img_CSV, script="""source activate pgm_pipeline
-				python -W ignore -u @var1@/norm.py @var2@ 99 $( getmetadata "custom_cpu" ) @out1@ 5000
-				source deactivate""")
-	mode_percent._custom("cpu") ="2"
+        val mode_percent = BashEvaluate(var1=prepro_pipeline_path, var2=img_CSV, script="""
+                                eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+                                conda activate pgm_pipeline
+                                python -W ignore -u @var1@/norm.py @var2@ 99 $( getmetadata "custom_cpu" ) @out1@ 10000
+                                conda deactivate""")
+        mode_percent._custom("cpu") ="2"
 
         // REGISTRATION
-        val registration = BashEvaluate(var1=prepro_pipeline_path, var2=img_CSV, param1="3", param2="rigid", param3="Nuclei", param4="DO", script="""source activate pgm_pipeline
+        val registration = BashEvaluate(var1=prepro_pipeline_path, var2=img_CSV, param1="3", param2="rigid", param3="Nuclei", param4="DO", script="""
+                              eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+                              conda activate pgm_pipeline
                               python -u @var1@/mainReg.py @var1@ @folder1@ @var2@ @param1@ @param2@ @param3@ @param4@ "1" "3" "noBspline"
-                              source deactivate """)
+                              conda deactivate """)
         val regImg_array = Folder2Array(registration.folder1)
         val reg_CSV = Array2CSV(regImg_array)
 	
@@ -61,20 +65,26 @@ object pipeline {
 				
 				// PREPROCESSING
 				info("Processing tile %s.....".format(r))
-				val prePro = BashEvaluate(var1=prepro_pipeline_path, var2=tile, var3=mode_percent.out1, param1="DO", script="""source activate pgm_pipeline
-						python -u @var1@/main.py @var1@ @var2@ @folder1@ @out1@ @out2@ $( getmetadata "custom_cpu" ) 0.05 @var3@ @out3@ @folder2@/candidates_max.h5 @param1@
-						source deactivate """) 
+                                val prePro = BashEvaluate(var1=prepro_pipeline_path, var2=tile, var3=mode_percent.out1, param1="DO", script="""
+                                                eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+                                                conda activate pgm_pipeline
+                                                python -u @var1@/main.py @var1@ @var2@ @folder1@ @out1@ @out2@ $( getmetadata "custom_cpu" ) 0.05 @var3@ @out3@ @folder2@/candidates_max.h5 @param1@
+                                                conda deactivate """)
 				prePro._custom("cpu") ="4"
 
 				// CANDIDATE PREDICTIONS
-				val predProb = BashEvaluate(var1=predProb_pipeline_path, var2=prePro.out1, script="""source activate pgm_pipeline
-						python @var1@/get_proba_DNN.py @var1@ @var2@ @out1@
-						source deactivate""")
+                                val predProb = BashEvaluate(var1=predProb_pipeline_path, var2=prePro.out1, script="""
+                                                eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+                                                conda activate pgm_pipeline
+                                                python @var1@/get_proba_DNN.py @var1@ @var2@ @out1@
+                                                conda deactivate""")
 
 				// GRAPH DECODING
-				val GM = BashEvaluate(var1=pgm_pipeline_path, var2=predProb.out1, var3=tagList, var4=prePro.out2, var5=prePro.out3, var6=predProb_pipeline_path, script="""source activate pgm_pipeline
-						python -u @var1@/main.py @var1@ @var2@ @out2@ @out1@ $( getmetadata "custom_cpu" ) 3 4 @out3@ blind @var3@ @var4@ @var5@ @var6@
-						source deactivate""")
+                                val GM = BashEvaluate(var1=pgm_pipeline_path, var2=predProb.out1, var3=tagList, var4=prePro.out2, var5=prePro.out3, var6=predProb_pipeline_path, script="""
+                                                eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+                                                conda activate pgm_pipeline
+                                                python -u @var1@/main.py @var1@ @var2@ @out2@ @out1@ $( getmetadata "custom_cpu" ) 3 4 @out3@ blind @var3@ @var4@ @var5@ @var6@
+                                                conda deactivate""")
 				GM._custom("cpu") = "1"
 
                                 val addTileOffset = BashEvaluate(var1=GM.out1, var2=StringInput("%s".format(r)), param1=tile_size_x, param2=tile_size_y, script="""
